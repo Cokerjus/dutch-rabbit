3590d8169bf8ca59605bc18de5a03f42eb2ad708
unknown <anumahjoshuaeneye@gmail.com>
2024-03-28 02:48:49 +0100

    jumbotron started

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { Jumbotron } from "./components";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <Jumbotron />
+    </div>
+  );
 }
--- src/components/Jumbotron.tsx
+++ src/components/Jumbotron.tsx
@@ -1,7 +1,8 @@
 function Jumbotron() {
   return (
-    <div className="bg-[linear-gradient(to_right_bottom,rgba(0,16,57,0.3),rgba(0,16,52,0.3)),url('/assets/bg1.png')] min-h-screen bg-center bg-cover bg-no-repeat">
-      hello world
+    <div className="bg-[linear-gradient(to_right_bottom,rgba(0,0,0,0.8),rgba(0,0,0,0.6)),url('/assets/bg2.png')] min-h-screen bg-center bg-cover bg-no-repeat relative">
+      <div className="absolute bottom-0 h-2/12" />
+      <h1 className="text-3xl font-bold underline">hello world</h1>
     </div>
   );
 }
