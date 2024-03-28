85e5da8cd0a7415d35b95cbae985baa1105a3d2b
unknown <anumahjoshuaeneye@gmail.com>
2024-03-28 02:36:52 +0100

    alice carousel add

---
+++ src/components/Jumbotron.tsx
@@ -0,0 +1,9 @@
+function Jumbotron() {
+  return (
+    <div className="bg-[linear-gradient(to_right_bottom,rgba(0,16,57,0.3),rgba(0,16,52,0.3)),url('/assets/bg1.png')] min-h-screen bg-center bg-cover bg-no-repeat">
+      hello world
+    </div>
+  );
+}
+
+export default Jumbotron;
