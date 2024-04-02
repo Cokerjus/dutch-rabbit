393a6772dd3a5f5f26672cafc6d5607bc855dcef
unknown <anumahjoshuaeneye@gmail.com>
2024-04-02 12:20:51 +0100

    footer responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,10 +5,10 @@ function Footer() {
   return (
     <div className="py-5 md:py-8 lg:py-12 border-t-2 ">
       <div className="max-w-screen-lg mx-auto">
-        <h3 className="text-white text-4xl pb-8 tracking-wider font-bold ">
+        <h3 className="text-white text-4xl pb-8 tracking-wider font-bold text-center md:text-left">
           Dutch Rabbit
         </h3>
-        <div className="flex flex-col md:flex-row md:justify-between items-end border-b-2 py-6">
+        <div className="flex flex-col md:flex-row md:justify-between items-center md:items-end border-b-2 py-6">
           <div className="flex gap-4 text-white">
             <p>Home</p>
             <p>Features</p>
@@ -16,7 +16,7 @@ function Footer() {
             <p>Roadmap</p>
             <p>Security Audit</p>
           </div>
-          <div className="flex flex-col items-end gap-3 text-white">
+          <div className="flex flex-col items-center md:items-end gap-3 text-white">
             <h3 className="text-white text-4xl pb-3 font-bold ">Contact Us</h3>
             <a href="admin@dutchrabbit.co">admin@dutchrabbit.co</a>
             <div className="flex gap-8">
@@ -27,7 +27,7 @@ function Footer() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col pt-6 gap-3 text-white items-start">
+        <div className="flex flex-col pt-6 gap-3 text-white items-center md:items-start">
           <p className="text-white">
             © {new Date().getFullYear()} Dutch Rabbit - All rights reserved.
           </p>
